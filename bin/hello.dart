db70cdcc831d9cf1efd764432afa35406fd1dde7
patipan-patisampita <patipan.patisampita@gmail.com>
2021-11-01 13:18:12 +0700

    initial commit

---
+++ bin/hello.dart
@@ -0,0 +1,11 @@
+import 'package:dartapp1/calculate.dart' as abc;
+
+void main() {
+  var name = 'Mark';
+  var age = 25;
+  //double length = 50;
+  //double width = 40;
+  print('$name $age');
+  //print('${length * width}');
+  print('${abc.calulate()}');
+}
